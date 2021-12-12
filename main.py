2f8326e4e9d6d3fa62da92524f1620b154c193ac
ÐDAKorotkovMIEM <dakortkov_1@miem.hse.ru>
2021-12-12 16:04:38 +0300

    test1

--- main.py
+++ main.py
@@ -46,3 +46,5 @@ if __name__ == '__main__':
                                  VALUES(?, ?);""", (user_id, role))
                     send_message(user_id, f'Поздравляю, можешь продолжать.')
                     conn.commit()
+
+#test
